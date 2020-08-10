c9eeb0e18428fcc9d694499f14a723b4c778651a
Nicholas Manning <nicholas.manning@gmail.com>
2020-08-10 11:09:40 -0400

    Replacing site with sapper site

--- android/app/src/main/kotlin/com/example/nickmanningdev/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.nickmanningdev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<style>
+	@font-face{
+	  font-family: 'Campton Book';
+	  src: url('/fonts/Rene-Bieder-Campton-Book.woff') format('woff');
+	}
+	h1, h2 {
+		font-family: 'Campton Book';
+		font-size: 52px;
+	}
+	h1 {
+		font-size: 52px;
+	}
+	h2 {
+		font-size: 24px;
+		margin: 40px 0px 15px 0px;
+	}
+	p {
+		font-family: 'Open Sans'
+	}
+	a {
+		color: #6C2FFE;
+		text-decoration: none;
+	}
+</style>
+
+<svelte:head>
+	<title>nickmaning.dev</title>
+</svelte:head>
+
+<div class="hero-section">
+	<h1>nickmanning.dev</h1>
+	<p>For the past 18 years, Nick has found nothing more fun than turning creative ideas into software. Before even owning a computer in middle school, Nick would copy down, by hand, video games written in BASIC into his daily journal because the code looked cool. In high school, he started teaching himself programming, becoming an obsessed "programming book enthusiast"</p>
+	<p>Nick has worked as technical lead on various projects for global brands such as Adidas, Reebok and Louis Vuitton. Since serving as Director of Mobile for a now Trip Advisor acquired company, he has co-found various startups. Since 2016, he has held various positions as CTO, including the Techstars accelerator.</p>
+	<h2>Links</h2>
+	<p><a href="https://fluttercrashcourse.com">fluttercrashcourse.com</a></p>
+	<p><a href="https://www.youtube.com/fluttercrashcourse">YouTube</a></p>
+	<p><a href="https://www.twitter.com/seenickcode">Twitter</a></p>
+</div>
