1097c2b53d8e4e0b4f6d50f724e33167d1cbfd8d
Nicholas Manning <nicholas.manning@gmail.com>
2020-08-10 11:17:46 -0400

    Adding files.

--- __sapper__/build/server/server.js
+++ __sapper__/build/server/server.js
@@ -3301,6 +3301,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'nickmanning-dev', // used for Github pages as repo name
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		middleware()
